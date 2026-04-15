557c58190b0042b6846ffacf3fff21c12245644b
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-04-15 11:15:04 -0300

    Add files via upload

--- trab.py
+++ trab.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.image("teste.png")
+st.image("Foto moana.png")
 st.write("Site Clara Ferreira")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/exerc%C3%ADcios")
