02ac5ff344dff52024cbcc0366f33499bfd57409
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-04-27 11:47:04 -0300

    Update trab.py

--- trab.py
+++ trab.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 
-st.image("Foto moana.png")
+st.image("Foto-empresa.png")
+st.link_image("https://www.starbucks.com.br/")
 st.write("Site Clara Ferreira")
 st.write("Clara Ferreira")
+st.write("Sou estudante do IFPB e, mesmo sem muita experiência, possuo grande vontade de apernder. Escolhi representar a aempresa acima, pois é onde muitos desejam se empregar.")
+st.write("Acessar meu site abaixo")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/exerc%C3%ADcios")
