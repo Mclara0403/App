6c00164b16fd916b5f5f3a4894492cb6f6884080
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-04-27 11:38:44 -0300

    trab.py

--- trab.py
+++ trab.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+
 st.image("Foto moana.png")
 st.write("Site Clara Ferreira")
+st.write("Clara Ferreira")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/exerc%C3%ADcios")
