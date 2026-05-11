f9c796587747a68dfb72d32b55c2d6ac513fc37f
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-05-11 11:54:08 -0300

    trab.py

--- trab.py
+++ trab.py
@@ -1,14 +1,4 @@
 import streamlit as st
-
-st.image("Foto-empresa.png")
-st.link_image("https://www.starbucks.com.br/")
-st.write("Site Clara Ferreira")
-st.write("Clara Ferreira")
-st.write("Sou estudante do IFPB e, mesmo sem muita experiência, possuo grande vontade de apernder. Escolhi representar a aempresa acima, pois é onde muitos desejam se empregar.")
-st.write("Acessar meu site abaixo")
-st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/exerc%C3%ADcios")
-
-import streamlit as st
 import base64
 
 # CONFIG
