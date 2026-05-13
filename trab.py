a501987b618df4cf9559c74abf2cc00833f91353
Mclara0403 <maria.ferreira.5@academico.ifpb.edu.br>
2026-05-13 12:04:24 -0300

    trab.py

--- trab.py
+++ trab.py
@@ -1,77 +1,30 @@
 import streamlit as st
-import base64
 
-# CONFIG
-st.set_page_config(page_title="Perfil", layout="wide")
-
-# TOPO (imagem clicável)
-col1, col2, col3 = st.columns([1,2,1])
+col1, col2, col3 = st.columns([1, 2, 3])
 
 with col2:
-    st.markdown(f"""
-        <div style="text-align: center; margin-bottom: 50px;">
-            <a href="https://www.starbucks.com.br/" target="_blank">
-                <img src="Foto-empresa.png" 
-                     width="320" 
-                     style="border-radius:12px;">
-            </a>
-        </div>
-    """, unsafe_allow_html=True)
+  st.image("Foto-empresa.png", width=300, link="https://www.starbucks.com.br/")
 
-# LAYOUT PRINCIPAL
-col_left, col_right = st.columns([3,1])
+st.title("Nome: Clara Ferreira")
 
-with col_left:
-    st.markdown("""
-    <div style='margin-bottom:30px; font-size:30px;'>
-        <b>Nome Maria Clara</b>
-    </div>
-    """, unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
-    # subcolunas
-    subcol1, subcol2 = st.columns([1,4])
+col1, col2 = st.columns(2)
 
-    # IMAGEM (centralizada verticalmente)
-    with subcol1:
-        st.markdown("""
-        <div style="
-            display: flex;
-            align-items: center;
-            height: 100%;
-        ">
-        """, unsafe_allow_html=True)
+with col1:
+  st.image("foto-clara.jpg", width=300)
+ 
+with col2:
+  st.header("Sobre Mim:")
+  st.write(" Leitora que insiste em se atriscar na vida e estudante do terceiro ano do curso técnico integrado em informática - campus Itabaiana ")
 
-        st.image("Foto moana.png", width=800)
+st.write("")
+ 
+st.link_button("Acessar Site", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/in%C3%ADcio")
 
-        st.markdown("</div>", unsafe_allow_html=True)
+st.markdown("<br>", unsafe_allow_html=True)
 
-    # TEXTO
-    with subcol2:
-        st.markdown("""
-        <div style="
-            text-align: justify;
-            font-size: 20px;
-            line-height: 2.0;
-            width: 100%;
-            max-width: none;
-        ">
-            <b>Sobre Maria Clara:<br>
-            Maria Clara é uma estudante do curso técnico em informática no IFPB campus Itabaiana e é uma leitora muito esforçada.
-        </div>
-        """, unsafe_allow_html=True)
+col1, col2, col3 = st.columns([1, 2, 3])
 
-    st.markdown("<div style='margin-top:30px;'>", unsafe_allow_html=True)
-    st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/site-m-clara-ferreira/exerc%C3%ADcios")
-    st.markdown("</div>", unsafe_allow_html=True)
-
-with col_right:
-    st.empty()
-
-# 🔥 NOVO BLOCO (WhatsApp clicável no final)
-st.markdown(f"""
-    <div style="text-align: center; margin-top: 10px;">
-        <a href="https://web.whatsapp.com/" target="_blank">
-            <img src="donwload.png" width="100">
-        </a>
-    </div>
-""", unsafe_allow_html=True)
+with col2:
+  st.image("download.png", width=100, link="https://wa.me/+5583986246096")
